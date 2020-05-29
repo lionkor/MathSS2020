4ca6ba78116cbce3adf3262ef18b2e9b5a009887
Lion Kortlepel <development@kortlepel.com>
2020-05-29 13:07:53 +0200

    Implement CKomplex

--- Praktikum_4/CKomplex.cpp
+++ Praktikum_4/CKomplex.cpp
@@ -1,1 +1,58 @@
 #include "CKomplex.h"
+#include <cmath>
+
+double CKomplex::abs() const {
+    return std::sqrt(m_re * m_re + m_im * m_im);
+}
+
+CKomplex CKomplex::operator-() const {
+    return CKomplex(-m_re, -m_im);
+}
+
+CKomplex CKomplex::operator+(const CKomplex& rhs) const {
+    return CKomplex(m_re + rhs.m_re, m_im + rhs.m_im);
+}
+
+CKomplex CKomplex::operator-(const CKomplex& rhs) const {
+    return *this + (-rhs); // delegate
+}
+
+CKomplex CKomplex::operator*(const CKomplex& rhs) const {
+    // (a+bi)(c+di) = (ac−bd) + (ad+bc)i
+    const auto& a = m_re;
+    const auto& b = m_im;
+    const auto& c = rhs.m_re;
+    const auto& d = rhs.m_im;
+
+    CKomplex result;
+    result.m_re = a * c - b * d;
+    result.m_im = a * d + b * c;
+    return result;
+}
+
+CKomplex& CKomplex::operator+=(const CKomplex& rhs) {
+    m_re += rhs.m_re;
+    m_im += rhs.m_im;
+    return *this;
+}
+
+CKomplex& CKomplex::operator-=(const CKomplex& rhs) {
+    return *this += (-rhs); // delegate
+}
+
+CKomplex& CKomplex::operator*=(const CKomplex& rhs) {
+    // compact version of operator*'s implementation
+    auto re_copy = m_re;
+    auto im_copy = m_im;
+    m_re         = re_copy * rhs.m_re - im_copy * rhs.m_im;
+    m_im         = re_copy * rhs.m_im + im_copy * rhs.m_re;
+    return *this;
+}
+
+CKomplex operator*(double scalar, const CKomplex& comp) {
+    return CKomplex(comp.m_re * scalar, comp.m_im * scalar);
+}
+
+CKomplex operator*(const CKomplex& comp, double scalar) {
+    return operator*(scalar, comp); // delegate
+}
--- Praktikum_4/CKomplex.h
+++ Praktikum_4/CKomplex.h
@@ -1,9 +1,35 @@
 #ifndef CKOMPLEX_H
 #define CKOMPLEX_H
 
-
 class CKomplex
 {
+private:
+    double m_im { 0 };
+    double m_re { 0 };
+
+    CKomplex() = default;
+
+public:
+    CKomplex(double _re, double _im)
+        : m_im(_im), m_re(_re) { }
+
+    double im() const { return m_im; }
+    double re() const { return m_re; }
+    void   set_im(double v) { m_im = v; }
+    void   set_re(double v) { m_re = v; }
+
+    double abs() const;
+
+    CKomplex  operator-() const;
+    CKomplex  operator+(const CKomplex& rhs) const;
+    CKomplex  operator-(const CKomplex& rhs) const;
+    CKomplex  operator*(const CKomplex& rhs) const;
+    CKomplex& operator+=(const CKomplex& rhs);
+    CKomplex& operator-=(const CKomplex& rhs);
+    CKomplex& operator*=(const CKomplex& rhs);
+
+    friend CKomplex operator*(double, const CKomplex&);
+    friend CKomplex operator*(const CKomplex&, double);
 };
 
 #endif // CKOMPLEX_H
